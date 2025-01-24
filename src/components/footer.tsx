34c97e51902c6b460473d55d9b9f4ca70e7dc54a
Pietro <beelbonacossa@gmail.com>
2025-01-23 21:07:34 -0300

    base creada

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-xs text-gray-500 pt-4 text-center">
+      © 2025 Subscribe Form. All Rights Reserved. Design by EquipoDotGuru.
+    </footer>
+  );
+};
+
+export default Footer;
